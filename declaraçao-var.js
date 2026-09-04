55fe84209dfd43b6bd97419adba83fdda0d6841d
BAIANO-157 <mongelucas815@gmail.com>
2026-09-04 17:01:20 -0300

    declaraçao-var.js

---
+++ declaraçao-var.js
@@ -0,0 +1,10 @@
+function exmploVar() {
+    var x = 10;
+    if (true) {
+       var x = 20; // memsma variavel x e redeclarada dentro do bloco
+       console.log(x); // 20 
+    }
+    console.log(x); // 20 (o valor foi alterado)
+}
+
+exmploVar();
