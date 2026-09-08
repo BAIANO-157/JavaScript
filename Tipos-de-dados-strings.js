236e6eab3e9757f60dc6c597cb293be6896c81b3
BAIANO-157 <mongelucas815@gmail.com>
2026-09-08 17:17:24 -0300

    add Tipos-de-dados-strings.js

---
+++ Tipos-de-dados-strings.js
@@ -0,0 +1,3 @@
+let nome = "Lucas"; // String com aspas duplas 
+let sobrenome = 'Paulo'; // String com aspas simples
+let saudacao = `Ola, ${nome}!`; // Template literal com interpolação de variaveis 
