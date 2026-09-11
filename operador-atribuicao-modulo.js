8f2dad43d3c84ddbd5202c7f222947a7cc9130e3
gabriela-reis-souza <mongelucas815@gmail.com>
2026-09-11 17:26:59 -0300

    add-operador-atribuicao-modulo.js

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totalDoces = 10;
+let crianças = 3;
+
+// Queremos saber quantos doces sobraram após dividir igualmente
+totalDoces %= crianças;
+
+console.log(totalDoces); // 1 -> pois 1 0dividido por 3 sobra 1 
