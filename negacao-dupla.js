10896d5aeff5a42bf5dac3556b4eae6405354285
BAIANO-157 <mongelucas815@gmail.com>
2026-09-15 17:05:14 -0300

    add-negacao-dupla.js

---
+++ negacao-dupla.js
@@ -0,0 +1,10 @@
+// valores falsy
+console.log(!!0);       // false
+console.log(!!"");      // false
+console.log(!!null);    // false   
+
+// Valores truthy
+console.log(!!1);        // true
+console.log(!!"texto");  // true
+console.log(!![]);       // true (Array vazios são objetos, logo são truthy)
+console.log(!!{});       // true (Objetos vazios são truthy)
