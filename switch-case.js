d62dab025877a2598e599579a8359b3c91593177
seriestiktok662-design <mongelucas815@gmail.com>
2026-09-22 15:45:27 -0300

    add-switch-case.js

---
+++ switch-case.js
@@ -0,0 +1,25 @@
+const classe = "Mago";
+switch (classe) {
+case "Guerreiro":
+console.log("'Classe: Guerreiro");
+console.log("Habitadades: Ataque poderoso Dores soda Forda de atenor e resistência.")
+console.log("habilidades; ateque poderoso, Defesa sólida, Furia de batalha");
+    break;
+case "Mago":
+console.Log("CLasse: Mago");
+console.log("O mago é um conjurador de feitiços, especializado em magia ofensiva e suporte.");
+console.log("Habilidades: Magia de fogo, Magia de gelo, Magia de cura");
+break;
+case "Arqueiro":
+console.Log("CLasse: Arqueiro");
+console.log("0 arqueiro é um atirador de Longa distância, especializado em precisão e agilidade.");
+console.Log("Habilidades: Precisão, Agilidade, Tiro com precisão");
+break;
+case "Ladino":
+console.Log("Classe: Ladino");
+console.Log("0 Ladino é um especialista em furtividade e trapas, utilizado para sabotagen e espionagem.");
+console.log("Habilidades: Furtividade, Trapas, Assassinio");
+break;
+default:
+console. Log ("CLasse desconhecida, Por favor, escolha entre guerreiro, mago, arqueiro ou ladino.");
+}
